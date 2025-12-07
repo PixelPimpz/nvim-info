d08bfbc9f01a7f31391d9aed205f63308cf58c99
lucifer-morningstar <slobeck@gmail.com>
2025-12-07 13:59:01 -0800

    basic push. code/fizes

--- scripts/get_cur_buffer.sh
+++ scripts/get_cur_buffer.sh
@@ -3,7 +3,7 @@ tmux display -p "get-cur-buffer called"
 main() {
   local SOCKS="/run/user/1000"
   local PANE_PID="$(tmux display -p "#{pane_pid}")"
-  tmux display -p "$("${SOCKS}")"
+  tmux display -p "SOCKS: ${SOCKS}"
   #local SOCKET=(ls ${SOCKS} | grep "${PANE_PID}"
   tmux display -p "PANE_PID: ${PANE_PID}"
   #tmux display -p "SOCKET: ${SOCKET}"
