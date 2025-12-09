5e9e140766da0f1fdcddb08306502342d29393be
lucifer-morningstar <slobeck@gmail.com>
2025-12-09 11:17:59 -0800

    basic push. code/fizes

--- scripts/get_cur_buffer.sh
+++ scripts/get_cur_buffer.sh
@@ -6,7 +6,7 @@ main() {
   local PANE_PID="$(tmux display -p "#{pane_pid}")"
   local PROC="$(ps -h --ppid "${PANE_PID}" -o cmd | awk '{print $1}')"  
   if [[ "${PROC}" == "nvim" ]]; then
-    local ICON=$(yq 'icons.nvim' ${ICONS} )
+    local ICON=$(yq '.icons.nvim' ${ICONS} )
     local SOCKET="/tmp/$(ls /tmp | grep -E "${PANE_PID}")"
     local BUF_NAME="$( nvim --server ${SOCKET} --remote-expr 'expand("%:t")' )"
     
