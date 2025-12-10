d3a3ea0b8a287a375b2ab9fdde08933c1e054e82
lucifer-morningstar <slobeck@gmail.com>
2025-12-10 15:35:19 -0800

    basic push. code/fizes

--- scripts/get_cur_buffer.sh
+++ scripts/get_cur_buffer.sh
@@ -45,7 +45,7 @@ set_status() {
 
 debug() {
   [[ "$1" ]] && local OUT="${1}" || local OUT="no data"
-  local FMT=' %10s | %-56s '
+  local FMT='%10s | %-56s'
   printf "${FMT}\n" "${OUT%:*}" "${OUT#*:}"
 }
 
