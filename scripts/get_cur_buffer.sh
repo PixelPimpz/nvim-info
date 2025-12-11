6ffdeceffd2398da89cf548ecadf98608b1af81e
lucifer-morningstar <slobeck@gmail.com>
2025-12-11 00:57:45 -0800

    basic push. code/fizes

--- scripts/get_cur_buffer.sh
+++ scripts/get_cur_buffer.sh
@@ -14,7 +14,7 @@ main() {
   local PROC="$(ps -h --ppid "${PANE_PID}" -o cmd | head  -1 | awk '{print $1}')"  
 
   #if [[ "${PROC}" == "nvim" ]]; then
-  if [[ "${SOCKET}" ~= $PANE_PID ]]; then
+  if [ "${SOCKET}" ~= ${PANE_PID} ]; then
 
     local YQ_EXIT=$?
     (( $YQ_EXIT != 0 )) && fatal "yq failed with code $YQ_EXIT. Check yaml for path & syntax."
