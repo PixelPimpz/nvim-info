f613f1f74dfc399ad344083362f9731a8293d704
lucifer-morningstar <slobeck@gmail.com>
2025-12-18 15:24:49 -0800

    basic push. code/fizes

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tmux set-hook -g pane-focus-in[0] 'tmux display "#{hook}" was called.'
-tmux set-hook -g after-new-window[1] 'tmux display "#{hook}" was called.'
+tmux set-hook -g pane-focus-in[0] 'tmux display -p "#{hook}" was called.'
+tmux set-hook -g after-new-window[1] 'tmux display -p "#{hook}" was called.'
 #tmux set-hook -g after-load-buffer[0] 'tmux display "#{hook}" was called.'
