b554fbbce6cc1a83325ce70a45a3fc27dec3ffc9
lucifer-morningstar <slobeck@gmail.com>
2025-12-19 11:45:28 -0800

    basic push. code/fizes

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #tmux set-hook -g pane-focus-in[0] 'source-file "${TMUX_ROOT}/tmux.conf"'
-tmux set-hook -g pane-focus-in[0] 'run "echo"WTF"'
+tmux set-hook -g pane-focus-in[0] 'echo"WTF"'
 tmux set-hook -g after-new-window[1] 'tmux display "#{hook}" was called.'
 tmux set-hook -g after-refresh-client[10] 'tmux source-file "${TMUX_ROOT}/tmux.conf"'
 tmux set-hook -g after-refresh-client[11] 'tmux display -p "${TMUX_ROOT}/tmux.conf reloaded'
