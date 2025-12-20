7f98d7cd844a638549ea20c9f2a75b4f10a0d55f
lucifer-morningstar <slobeck@gmail.com>
2025-12-19 18:53:02 -0800

    basic push. code/fizes

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
-#tmux set-hook -g pane-focus-in[0] 'source-file "${TMUX_ROOT}/tmux.conf"'
+p!/usr/bin/env bash
 tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
-tmux set-hook -g after-new-window[1] 'display "#{hook}" was called.'
+tmux set-hook -g after-new-window[1] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
 tmux set-hook -g after-refresh-client[11] 'display -p "${TMUX_ROOT}/tmux.conf reloaded'
-#tmux set-hook -g after-load-buffer[0] 'tmux display "#{hook}" was called.'
