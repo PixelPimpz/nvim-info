04f892228070abe7d01f1eabf3b2be2ff4e16ed4
lucifer-morningstar <slobeck@gmail.com>
2025-12-20 08:25:27 -0800

    basic push. code/fizes

--- scripts/getIcon.sh
+++ scripts/getIcon.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+ICONS="$( tmux display-message -p "#{@LIB_ICON}" )"
+YQBIN="/usr/bin/yq"
+
+if ! command -v "${YQBIN}" &> /dev/null; then
+  fatal "yq executable not found at ${YQBIN}."
+fi
+
+tmux display "$ICONS"
+
+main() {
+
+}
+
+fatal() {
+  local out="$1"
+  printf 'ERROR: %s \n' $out
+  exit 1
+}
