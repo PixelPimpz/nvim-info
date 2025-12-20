4520d82f74ffe692950ed28f36c8d5080ec99e9a
lucifer-morningstar <slobeck@gmail.com>
2025-12-20 07:29:31 -0800

    basic push. code/fizes

--- scripts/getIcon.sh
+++ scripts/getIcon.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-local icons="$(tmux display "#{@LIB_ICON}" )"
-tmux display -p "#{@LIB_ICON}"
-printf 'icons: %s', "test-string"
+local icons="$(tmux display -p "#{@LIB_ICON}" )"
+echo "$icons"
