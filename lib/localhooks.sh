7ad3960ffd7ac53818c84c9a9e5b20dfbb2fb90e
lucifer-morningstar <slobeck@gmail.com>
2025-12-21 13:22:40 -0800

    basic push. code/fizes

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -2,4 +2,4 @@ p!/usr/bin/env bash
 tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
 tmux set-hook -g after-new-window[1] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
 tmux set-hook -g after-refresh-client[11] 'display -p "${TMUX_ROOT}/tmux.conf reloaded'
-tmux set-hook -g pane-died[0] 'display "pane: #{pane_pid}" has exited.'
+tmux set-hook -g pane-died[0] 'display -p "pane: #{pane_pid}" has exited.'
