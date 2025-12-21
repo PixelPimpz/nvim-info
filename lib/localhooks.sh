836d760b17159962f13ad13eb3e3ef4a46b0d205
lucifer-morningstar <slobeck@gmail.com>
2025-12-21 14:50:52 -0800

    basic push. code/fizes

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -1,6 +1,6 @@
 /bin/env bash
 tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
 tmux set-hook -g session-window-changed[0] 'display -p "Session-window-changed."'
+tmux set-hook -g session-window-changed[1] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
 tmux set-hook -g after-new-window[1] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh'
 tmux set-hook -g after-refresh-client[11] 'display -p "${TMUX_ROOT}/tmux.conf reloaded'
-tmux set-hook -g window-close :W 'display -p "window has exited."'
