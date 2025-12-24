a688346dbca523ca20c67a45d116ffe3c1ddf1d5
lucifer-morningstar <slobeck@gmail.com>
2025-12-24 12:47:02 -0800

    basic push. code/fizes

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env b
-tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh"' \; 'refresh-client'
+#!/usr/bin/env bash
+tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/scripts/get_cur_buffer.sh \; refresh-client'
 tmux set-hook -g session-created[0] 'run "#{@PLUG_ROOT}/get_cur_buffer"'
 tmux set-hook -g session-window-changed[0] 'run "#{@PLUG_ROOT}/get_cur_buffer"'
 tmux set-hook -g after-new-window[1] 'run "#{@PLUG_ROOT}/get_cur_buffer"'
