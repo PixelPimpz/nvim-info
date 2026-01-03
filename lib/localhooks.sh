30f077e77de34f7585eef4c517821f8c30b56af4
lucifer-morningstar <slobeck@gmail.com>
2026-01-03 05:38:44 -0800

    Generic commit

--- lib/localhooks.sh
+++ lib/localhooks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/tmux-nvim-info' 
+tmux set-hook -g pane-focus-in[0] 'run "#{@PLUG_ROOT}/tmux-nvim-info.tmux' 
 tmux set-hook -g pane-focus-in[1] 'refresh-client -S' 
-tmux set-hook -g pane-exited[0] 'run "#{@PLUG_ROOT}/tmux-nvim-info"'
+tmux set-hook -g pane-exited[0] 'run "#{@PLUG_ROOT}/tmux-nvim-info.tmux"'
