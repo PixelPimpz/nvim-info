ac5be120560539bb06aa45920facf1503ceeb029
lucifer-morningstar <slobeck@gmail.com>
2026-01-06 16:23:09 -0800

    Generic commit

--- scripts/localkeys.sh
+++ scripts/localkeys.sh
@@ -3,7 +3,7 @@ main() {
   for orig in "$( tmux list-keys | grep 'next-window' | grep -iv 'wheel' )"; do
     local keystr="$( awk '{print $4}' <<< $orig )"
     local cmdstr="$( awk '{print $5}' <<< $orig )"
-    echo "$keystr $cmdstr" >> ~/temp-tmux.log
+    echo -n "$keystr $cmdstr" >> ~/temp-tmux.log
   done
 }
 main
