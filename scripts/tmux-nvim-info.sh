ae6dc95ab1f8890627ecfe9bbc08dd833dab9c33
lucifer-morningstar <slobeck@gmail.com>
2026-01-18 16:12:43 -0800

    Generic commit

--- scripts/tmux-nvim-info.sh
+++ scripts/tmux-nvim-info.sh
@@ -43,6 +43,6 @@ main()
   ## set status bar 
   tmux set -g @nvim-info "${STATUS}"
   #tmux set -g @nvim-info-unit "#[fg=#{@Dark4}]#{E:@TriangleL}#[bg=#{@Light0_S}]#[reverse]#{@nvim-info} #[bg=default]#[noreverse]#{E:@TriangleRInverse}"
-  tmux set -g @nvim-info-unit "#[fg=#{@Dark4},default]#[push-default]#{E:@TriangleL}#[bg=#{@Light0_S}]#[reverse]#{@nvim-info} #[bg=default]#[noreverse]#[pop-default]#{E:@TriangleRInverse}"
+  tmux set -g @nvim-info-unit "#[fg=#{@Dark4}]#[push-default]#{E:@TriangleL}#[bg=#{@Light0_S}]#[reverse]#{@nvim-info} #[bg=#{@Dark0_H}]#[noreverse]#[pop-default]#{E:@TriangleRInverse}"
 }
 main
